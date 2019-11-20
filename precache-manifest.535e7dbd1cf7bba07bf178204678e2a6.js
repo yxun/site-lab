2ba992c583a9d18e235a9e57aa894c231470b428
yxun <yuanlin.yxu@gmail.com>
2019-11-19 21:12:43 -0500

    Updates

---
+++ precache-manifest.535e7dbd1cf7bba07bf178204678e2a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deff72579b42b3008ceb7dfb6f965a82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3e7820af2684653b703",
+    "url": "/static/css/main.afb0d04b.chunk.css"
+  },
+  {
+    "revision": "d4ef8b874adf79e50124",
+    "url": "/static/js/2.10fedaf8.chunk.js"
+  },
+  {
+    "revision": "d3e7820af2684653b703",
+    "url": "/static/js/main.fad3e42d.chunk.js"
+  },
+  {
+    "revision": "974f221da50f1486b200",
+    "url": "/static/js/runtime-main.8e127e9f.js"
+  }
+]);
