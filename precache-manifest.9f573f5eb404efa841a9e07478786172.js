42c21e23aa23806483d36e73b5e1d708081a711a
yxun <yuanlin.yxu@gmail.com>
2019-11-19 21:25:36 -0500

    Updates

---
+++ precache-manifest.9f573f5eb404efa841a9e07478786172.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39481b8f4aa5df9c7517a22c5ab742bc",
+    "url": "/yxun/site-lab/index.html"
+  },
+  {
+    "revision": "d3e7820af2684653b703",
+    "url": "/yxun/site-lab/static/css/main.afb0d04b.chunk.css"
+  },
+  {
+    "revision": "d4ef8b874adf79e50124",
+    "url": "/yxun/site-lab/static/js/2.10fedaf8.chunk.js"
+  },
+  {
+    "revision": "d3e7820af2684653b703",
+    "url": "/yxun/site-lab/static/js/main.fad3e42d.chunk.js"
+  },
+  {
+    "revision": "61bab2433fda5d2233d9",
+    "url": "/yxun/site-lab/static/js/runtime-main.d3cef24b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.535e7dbd1cf7bba07bf178204678e2a6.js"
+  "/yxun/site-lab/precache-manifest.9f573f5eb404efa841a9e07478786172.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yxun/site-lab/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
