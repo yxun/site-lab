cee75cf5dd8a6173274f08aa5ba07adb5d26d80e
yxun <yuanlin.yxu@gmail.com>
2019-11-30 17:47:48 -0500

    Content Updates

--- src/components/SideCard.js
+++ src/components/SideCard.js
@@ -13,11 +13,11 @@ const SideCard = () => (
         <Card>
             <CardImg top width="100%" src={banner} alt="banner" />
             <CardBody>
-                <CardTitle className="h3 mb-2 pt-2 font-weight-bold text-secondary">Yuanlin Xu</CardTitle>
+                <CardTitle className="h3 mb-2 pt-2 font-weight-bold text-secondary">Site Lab</CardTitle>
                 <CardSubtitle className="text-secondary mb-3 font-weight-light text-uppercase" 
-                    style={{ fontSize: '0.8rem' }}>Developer</CardSubtitle>
+                    style={{ fontSize: '0.8rem' }}>Try and Learn</CardSubtitle>
                 <CardText className="text-secondary mb-4" 
-                    style={{ fontSize: '0.75rem' }}>Full-stack Learning</CardText>
+                    style={{ fontSize: '0.75rem' }}>Work In Progress</CardText>
                 <Button color="info" className="font-weight-bold" outline><a href="https://github.com/yxun">View Profile</a></Button>
             </CardBody>
         </Card>
